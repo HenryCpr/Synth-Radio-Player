15e01ad3e23ba194ea9c18ca2d69ecb3272c093b
mrrmx <alexterranova81@gmail.com>
2022-02-15 02:26:36 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -117,7 +117,7 @@ body {
   background-color: transparent;
   background-size: 5px 5px, 5px 5px;
   background-position: -1px -1px, -1px -1px;
-  background-image: linear-gradient(rgba(255, 255, 255, 0.1) 1px ,transparent 1px), linear-gradient(90deg, rgba(255, 255, 255, 0.1) 1px, transparent 1px);
+  background-image: linear-gradient(rgba(255, 255, 255, 0.1) 1px ,transparent 1px) /*linear-gradient(90deg, rgba(255, 255, 255, 0.1) 1px, transparent 1px)*/;
   
   z-index: 1;
 }
